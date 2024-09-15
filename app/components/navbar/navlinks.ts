6a7ddacdc9755a5fbd5fe4779d2e2a334fb16ce0
Achal <achaldev3@gmail.com>
2024-09-15 23:32:05 +0530

    add desktop navbar

---
+++ app/components/navbar/navlinks.ts
@@ -0,0 +1,44 @@
+interface navLinkFormat {
+    id: string;
+    label: string;
+    link: string;
+    last?: boolean;
+  }
+
+  type navLink = navLinkFormat[];
+
+const NAV_LINKS: navLink = [
+    {
+      id: "home",
+      label: "Home",
+      link: "#",
+    },
+    {
+      id: "product",
+      label: "Products",
+      link: "#",
+    },
+    {
+      id: "innovation",
+      label: "Innovations",
+      link: "#",
+    },
+    {
+      id: "news",
+      label: "News",
+      link: "#",
+    },
+    {
+      id: "about",
+      label: "About Us",
+      link: "#",
+    },
+    {
+      id: "contact",
+      label: "Contact Us",
+      link: "#",
+      last: true,
+    },
+  ];
+
+  export default NAV_LINKS;
