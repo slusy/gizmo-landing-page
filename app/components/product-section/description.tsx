f8afca5c57804fa614f59f29541abb57fe509f43
Achal <achaldev3@gmail.com>
2024-09-27 22:54:45 +0530

    testimonial section

--- app/components/product-section/description.tsx
+++ app/components/product-section/description.tsx
@@ -6,7 +6,7 @@ export const ProductSectionDescription: React.FC = () => {
       <h1 className="text-4xl md:text-5xl text-[#2A2A2A] font-bold">
         Featured <span className="text-[#3064E8]">Products</span>
       </h1>
-      <p className="lg:px-48 mt-6 text-[#5E5E5E] text-sm">
+      <p className="lg:px-48 mt-6 text-[#5E5E5E] text-sm lg:text-base">
         Discover our top picks of innovative hardware solutions designed to
         enhance your everyday life. From cutting-edge smart home devices to the
         latest in wearable technology, explore how these products can transform
---
+++ app/components/testimonial-section/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TestimonialSectionDescription } from "./description";
+
+export const TestimonialSection: React.FC = () => {
+  return (
+    <div className="py-8">
+      <TestimonialSectionDescription />
+    </div>
+  );
+};
