b711128487fa6277349c2e24268086705e80d7a6
Achal <achaldev3@gmail.com>
2024-09-28 21:27:33 +0530

    add testimonials

--- app/components/testimonial-section/index.tsx
+++ app/components/testimonial-section/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { TestimonialSectionDescription } from "./description";
+import { Testimonial } from "./testimonial";
 
 export const TestimonialSection: React.FC = () => {
   return (
     <div className="py-8">
       <TestimonialSectionDescription />
+      <Testimonial />
     </div>
   );
 };
