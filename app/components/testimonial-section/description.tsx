d94515e092d7dc4fb150925e0cbb0d082f46168b
Achal <achaldev3@gmail.com>
2024-10-25 11:44:43 +0530

    resolve build conflict

--- app/components/testimonial-section/description.tsx
+++ app/components/testimonial-section/description.tsx
@@ -9,10 +9,10 @@ export const TestimonialSectionDescription: React.FC = () => {
         <span className="text-[#3064E8]">Customers</span> Say
       </h1>
       <p className="lg:px-48 mt-6 text-[#5E5E5E] text-sm lg:text-base">
-        Don't just take our word for it—see what our satisfied customers have to
-        say about their experiences with our innovative products. From immersive
-        VR adventures to seamless smart home control, our products are making a
-        difference in everyday lives.
+        Don&apos;t just take our word for it—see what our satisfied customers
+        have to say about their experiences with our innovative products. From
+        immersive VR adventures to seamless smart home control, our products are
+        making a difference in everyday lives.
       </p>
     </div>
   );
